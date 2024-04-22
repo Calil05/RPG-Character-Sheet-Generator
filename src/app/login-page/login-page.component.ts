10d088be41b5fb3cc9ba2691095d0926d2d43385
Gabriel Calil <gabrielbcalil@gmail.com>
2024-04-22 16:07:50 -0300

    wip: user register function

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { UserInterface } from '../interfaces/user-interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -13,9 +15,33 @@ export class LoginPageComponent {
     passwd: ''
   };
 
-  constructor(private authService: AuthService, private router: Router) { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { 
+
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: this.passwordMatchValidator
+    });
+
+  }
 
   option: string = 'login';
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password');
+    const confirmPassword = formGroup.get('confirmPassword');
+
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPassword.setErrors(null);
+    }
+  }
 
   toggleOption(selectedOption: string) {
     this.option = selectedOption
@@ -33,6 +59,23 @@ export class LoginPageComponent {
         console.error("ERROR! ", error)
       }
     );
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const formData = this.registerForm.value;
+      delete formData.confirmPassword;
+
+      this.authService.signup(formData).subscribe(
+        (response) => {
+          console.warn("User registered successfully!");
+          // Redirect to login page or wherever you want
+        },
+        (error) => {
+          console.error("ERROR! ", error);
+        }
+      );
+    }
   }
 
 }
