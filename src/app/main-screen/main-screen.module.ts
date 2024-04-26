156e054977a1b5abe7fca35c4db2f3cf708c7387
Gabriel Calil <gabrielbcalil@gmail.com>
2024-04-26 14:34:17 -0300

    wip: sidebar

--- src/app/main-screen/main-screen.module.ts
+++ src/app/main-screen/main-screen.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainScreenComponent } from './main-screen.component';
-
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [MainScreenComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbDropdownModule
   ],
   exports: [
     MainScreenComponent
